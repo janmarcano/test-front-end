2f6b31a13a2b94a5aad263024170b3c82bbe809c
Unknown <janmarcano@gmail.com>
2018-03-22 04:53:37 -0300

    Cambios a Dependencias

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ var OUT = path.resolve(__dirname, 'build');
 
 var html = new htmlPlugin({
     hash: true,
-    filename: '../index.html',
     template: 'src/index.html',
     favicon: 'src/favicon.ico'
 });
@@ -15,14 +14,15 @@ var html = new htmlPlugin({
 var config = {
     entry: SRC + '/index.js',
     output: {
-        path: OUT + '/app',
-        filename: 'bundle.js',
-        publicPath: '/app/'
+        path: OUT,
+        filename: 'app/bundle.js',
+        publicPath: '/'
     },
     module: {
         rules: [
             { test: /\.js?/, exclude: /node_modules/, loader: 'babel-loader' },
-            { test: /\.scss?/, exclude: /node_modules/, loader: ['style-loader','css-loader','sass-loader'] }
+            { test: /\.scss?/, exclude: /node_modules/, loader: ['style-loader','css-loader','sass-loader'] },
+            { test: /\.(png|svg)$/, use: [{ loader: 'file-loader', options: { name: 'assets/[name]-[hash].[ext]'}}] }
         ]
     },
     plugins: [
